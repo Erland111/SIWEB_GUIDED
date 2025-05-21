5cad8dcf7dadae73257aaf65d7cdfd4504b6818f
Erland111 <gianeland543@gmail.com>
2025-05-21 18:32:10 +0700

    saya tambah sql

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -2,6 +2,17 @@
 // It describes the shape of the data, and what data type each property should accept.
 // For simplicity of teaching, we're manually defining these types.
 // However, these types are generated automatically if you're using an ORM such as Prisma.
+// Di paling atas file, misal pakai Zod
+import { z } from 'zod';
+
+// Tambahkan ini sebelum export function
+export const UpdateInvoice = z.object({
+  customerId: z.string().uuid(),
+  amount: z.string(),
+  status: z.string(),
+});
+
+
 export type User = {
   id: string;
   name: string;
